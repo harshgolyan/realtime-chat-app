eb093f2b48b629930fc079af1dcc169bf03e8c99
harshgolyan <harshgolyan308@gmail.com>
2024-06-30 23:05:12 +0530

    login page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import SignUp from "./components/auth/signup";
+import Login from "./components/auth/login";
 
 const App = () => {
   return(
     <>
       <div className="bg-[#eaa7f8] h-screen">
-        <SignUp />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path = "/" element={<SignUp />}/>
+            <Route path = "/login" element={<Login />}/>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
--- frontend/src/components/auth/login.jsx
+++ frontend/src/components/auth/login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SignUp = () => {
+const Login = () => {
     return(
         <>
             <div className="flex items-center justify-center min-h-screen">
@@ -9,22 +9,22 @@ const SignUp = () => {
                         
                     </div>
                     <div className="bg-white flex-col rounded-r-lg w-[50%]">
-                        <div className="font-extrabold ml-[30%] text-[2rem]">Sign Up</div>
-                        <div className="mx-3 flex flex-col mt-4">
-                             <label className="pl-1 font-semibold" htmlFor="name">Name</label>
-                            <input className="border-2 rounded-md border-black pl-2" type="text" placeholder="enter your name..."/>
-                        </div>
-                         <div className="mx-3 flex flex-col mt-4">
+                        <div className="font-extrabold ml-[30%] text-[2rem]">Log In</div>
+                         <div className="mx-3 flex flex-col mt-6">
                              <label className="pl-1 font-semibold" htmlFor="name">Email</label>
                             <input className="border-2 rounded-md border-black pl-2" type="email" placeholder="enter your email..."/>
                         </div>
-                         <div className="mx-3 flex flex-col mt-4">
+                         <div className="mx-3 flex flex-col mt-6">
                              <label className="pl-1 font-semibold" htmlFor="name">Password</label>
                             <input className="border-2 rounded-md border-black pl-2" type="password" placeholder="enter your password"/>
                         </div>
-                        <div className="mx-3 mt-4 text-blue-700 underline ml-[7.5rem]"><a href="#">Already have an account ?</a></div>
-                        <div className="mx-3 mt-8">
-                            <button className="bg-blue-700 w-[100%] rounded-md p-2 text-white font-bold text-[1rem]">Sign Up</button>
+                        <div className="mx-3 mt-6 text-blue-700 underline ml-[10.5rem]"><a href="#">Create an Account !</a></div>
+                        <div className="mx-3 mt-[3rem]">
+                            <input className="" type="checkbox" />
+                            <label className="ml-1 font-medium" htmlFor="checkbox">I accept all terms & conditions.</label>
+                        </div>
+                        <div className="mx-3">
+                            <button className="bg-blue-700 w-[100%] rounded-md p-2 text-white font-bold text-[1rem]">Log In</button>
                         </div>
                     </div>
                 </div>
@@ -33,4 +33,4 @@ const SignUp = () => {
     )
 }
 
-export default SignUp;
+export default Login;
