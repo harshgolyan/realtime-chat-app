81a9608378a25a801b439565846aa5cedc975e32
harshgolyan <harshgolyan308@gmail.com>
2024-07-01 10:47:25 +0530

    sign up and login functionality

--- frontend/src/components/auth/login.jsx
+++ frontend/src/components/auth/login.jsx
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    // console.log(email, password)
+    const navigate = useNavigate();
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3000/login", {
+            email, password
+        }, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+        if (response.data.msg === 'user login successfully') {
+            localStorage.setItem('jwt',response.data.token)
+            navigate('/fetchchat');
+        }
+        })
+        .catch(error => {
+            console.error('There was an error!', error);
+        });
+    }
+
     return(
         <>
-            <div className="flex items-center justify-center min-h-screen">
-                <div className="h-[25rem] w-[40rem] rounded-lg flex">
-                    <div className="bg-purple-500 flex-col rounded-l-lg w-[50%]">
-                        
-                    </div>
-                    <div className="bg-white flex-col rounded-r-lg w-[50%]">
-                        <div className="font-extrabold ml-[30%] text-[2rem]">Log In</div>
-                         <div className="mx-3 flex flex-col mt-6">
-                             <label className="pl-1 font-semibold" htmlFor="name">Email</label>
-                            <input className="border-2 rounded-md border-black pl-2" type="email" placeholder="enter your email..."/>
+            <form onSubmit={onSubmitHandler}>
+                <div className="flex items-center justify-center min-h-screen">
+                    <div className="h-[25rem] w-[40rem] rounded-lg flex">
+                        <div className="bg-purple-500 flex-col rounded-l-lg w-[50%]">
+                            
                         </div>
-                         <div className="mx-3 flex flex-col mt-6">
-                             <label className="pl-1 font-semibold" htmlFor="name">Password</label>
-                            <input className="border-2 rounded-md border-black pl-2" type="password" placeholder="enter your password"/>
-                        </div>
-                        <div className="mx-3 mt-6 text-blue-700 underline ml-[10.5rem]"><a href="#">Create an Account !</a></div>
-                        <div className="mx-3 mt-[3rem]">
-                            <input className="" type="checkbox" />
-                            <label className="ml-1 font-medium" htmlFor="checkbox">I accept all terms & conditions.</label>
-                        </div>
-                        <div className="mx-3">
-                            <button className="bg-blue-700 w-[100%] rounded-md p-2 text-white font-bold text-[1rem]">Log In</button>
+                        <div className="bg-white flex-col rounded-r-lg w-[50%]">
+                            <div className="font-extrabold ml-[30%] text-[2rem]">Log In</div>
+                            <div className="mx-3 flex flex-col mt-6">
+                                <label className="pl-1 font-semibold" htmlFor="name">Email</label>
+                                <input className="border-2 rounded-md border-black pl-2" type="email" placeholder="enter your email..." onChange={(e) => setEmail(e.target.value)}/>
+                            </div>
+                            <div className="mx-3 flex flex-col mt-6">
+                                <label className="pl-1 font-semibold" htmlFor="name">Password</label>
+                                <input className="border-2 rounded-md border-black pl-2" type="password" placeholder="enter your password" onChange={(e) => setPassword(e.target.value)}/>
+                            </div>
+                            <div className="mx-3 mt-6 text-blue-700 underline ml-[10.5rem]"><a href="/">Create an Account !</a></div>
+                            <div className="mx-3 mt-[3rem]">
+                                <input className="" type="checkbox" />
+                                <label className="ml-1 font-medium" htmlFor="checkbox">I accept all terms & conditions.</label>
+                            </div>
+                            <div className="mx-3">
+                                <button className="bg-blue-700 w-[100%] rounded-md p-2 text-white font-bold text-[1rem]">Log In</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </>
     )
 }
--- frontend/src/components/auth/signup.jsx
+++ frontend/src/components/auth/signup.jsx
@@ -1,34 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    // console.log(name, email, password);
+    const navigate = useNavigate();
+
+    // const onSubmitHandler = (e) =>{
+    //     e.preventDefault()
+    //     axios('http://localhost:5173/',{
+    //         method:'post',
+    //         headers:{
+    //             'Content-Type':'application/json'
+    //         },
+    //         body:JSON.stringify({name,email,password})
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         if(data.msg === 'new user added successfully'){
+    //             navigate('/login')
+    //         }
+    //     })
+
+    // }
+    const onSubmitHandler = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/signup', {
+        name,
+        email,
+        password
+    }, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.data.msg === 'new user added successfully') {
+            navigate('/login');
+        }
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+};
+3   
+
     return(
         <>
-            <div className="flex items-center justify-center min-h-screen">
-                <div className="h-[25rem] w-[40rem] rounded-lg flex">
-                    <div className="bg-purple-500 flex-col rounded-l-lg w-[50%]">
-                        
-                    </div>
-                    <div className="bg-white flex-col rounded-r-lg w-[50%]">
-                        <div className="font-extrabold ml-[30%] text-[2rem]">Sign Up</div>
-                        <div className="mx-3 flex flex-col mt-4">
-                             <label className="pl-1 font-semibold" htmlFor="name">Name</label>
-                            <input className="border-2 rounded-md border-black pl-2" type="text" placeholder="enter your name..."/>
+            <form onSubmit={onSubmitHandler}>
+                <div className="flex items-center justify-center min-h-screen">
+                    <div className="h-[25rem] w-[40rem] rounded-lg flex">
+                        <div className="bg-purple-500 flex-col rounded-l-lg w-[50%]">
+                            
                         </div>
-                         <div className="mx-3 flex flex-col mt-4">
-                             <label className="pl-1 font-semibold" htmlFor="name">Email</label>
-                            <input className="border-2 rounded-md border-black pl-2" type="email" placeholder="enter your email..."/>
-                        </div>
-                         <div className="mx-3 flex flex-col mt-4">
-                             <label className="pl-1 font-semibold" htmlFor="name">Password</label>
-                            <input className="border-2 rounded-md border-black pl-2" type="password" placeholder="enter your password"/>
-                        </div>
-                        <div className="mx-3 mt-4 text-blue-700 underline ml-[7.5rem]"><a href="#">Already have an account ?</a></div>
-                        <div className="mx-3 mt-8">
-                            <button className="bg-blue-700 w-[100%] rounded-md p-2 text-white font-bold text-[1rem]">Sign Up</button>
+                        <div className="bg-white flex-col rounded-r-lg w-[50%]">
+                            <div className="font-extrabold ml-[30%] text-[2rem]">Sign Up</div>
+                            <div className="mx-3 flex flex-col mt-4">
+                                <label className="pl-1 font-semibold" htmlFor="name">Name</label>
+                                <input className="border-2 rounded-md border-black pl-2" type="text" placeholder="enter your name..." onChange={(e) => setName(e.target.value)}/>
+                            </div>
+                            <div className="mx-3 flex flex-col mt-4">
+                                <label className="pl-1 font-semibold" htmlFor="name">Email</label>
+                                <input className="border-2 rounded-md border-black pl-2" type="email" placeholder="enter your email..." onChange={(e) => setEmail(e.target.value)}/>
+                            </div>
+                            <div className="mx-3 flex flex-col mt-4">
+                                <label className="pl-1 font-semibold" htmlFor="name">Password</label>
+                                <input className="border-2 rounded-md border-black pl-2" type="password" placeholder="enter your password" onChange={(e) => setPassword(e.target.value)}/>
+                            </div>
+                            <div className="mx-3 mt-4 text-blue-700 underline ml-[7.5rem]"><a href="/login">Already have an account ?</a></div>
+                            <div className="mx-3 mt-8">
+                                <button className="bg-blue-700 w-[100%] rounded-md p-2 text-white font-bold text-[1rem]">Sign Up</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </>
     )
 }
