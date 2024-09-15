d2d69e7e951139aa85a0703727d2b21a71d7586c
harshgolyan <harshgolyan308@gmail.com>
2024-09-15 21:28:36 +0530

    ui and deploy

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const color = require("colors")
 const dotenv = require("dotenv")
+const path = require("path")
 
 const app = express()
 dotenv.config()
@@ -15,6 +16,23 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/user'))
 app.use(require('./routes/chat'))
 app.use(require('./routes/message'))
+
+// |-------------------------- deployment -----------------------------|
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV) {
+    app.use(express.static(path.join(__dirname1,'../frontend/dist')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname1,'../frontend/dist/index.html'))
+    })
+}
+else {
+    app.get("/", (req, res) => {
+        res.send("api is running")
+    })
+
+}
+
 
 
 const connectDB = async () => {
--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -6,45 +6,49 @@ const router = express.Router()
 
 //create chat
 router.post("/createchat", requireLogin, async (req, res) => {
-    const userId = req.body.userId
+    const userId = req.body.userId;
 
-    if(!userId) {
-        res.status(400).json({error:"invalid user id"})
+    if (!userId) {
+        return res.status(400).json({ error: "invalid user id" });
     }
-    var isChat = await Chat.find({
-        isGroupChat: false,
-        $and:[{
-            users:{$elemMatch: {$eq: req.user._id}}
-        },{
-            users:{$elemMatch: {$eq: userId}}
-        }]
-    })
-    .populate("users", "-password")
-    .populate("latestMessage")
 
-    isChat = await User.populate(isChat, {
-        path: "latestMessage.sender",
-        select: "name pic email"
-    })
-    
-    if(isChat.length > 0) {
-        res.send(isChat[0])
-    }
-    else {
-        var chatData = {
+    try {
+        let isChat = await Chat.find({
+            isGroupChat: false,
+            $and: [
+                { users: { $elemMatch: { $eq: req.user._id } } },
+                { users: { $elemMatch: { $eq: userId } } }
+            ]
+        })
+        .populate("users", "-password")
+        .populate("latestMessage");
+
+        isChat = await User.populate(isChat, {
+            path: "latestMessage.sender",
+            select: "name pic email"
+        });
+
+        if (isChat.length > 0) {
+            return res.send(isChat[0]);
+        }
+
+        const chatData = {
             chatName: "sender",
             isGroupChat: false,
             users: [req.user._id, userId]
-        }
-        try {
-            const createdChat = await Chat.create(chatData)
-            const fullChat = await Chat.findOne({_id : createdChat._id}).populate("users", "-password")
-            res.status(200).json(fullChat)
-        } catch (error) {
-            console.log(error)
-        }
+        };
+
+        const createdChat = await Chat.create(chatData);
+        const fullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "-password");
+
+        res.status(200).json(fullChat);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
     }
-})
+});
+
 
 //fetch chat of login user
 router.get("/fetchchat", requireLogin, (req, res) => {
